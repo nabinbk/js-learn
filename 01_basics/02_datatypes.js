c5526c1ff723ab96b581b1f38081b4f55e029f5a
nabinbk <113597176+nabinbk@users.noreply.github.com>
2025-02-14 17:24:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strick"; //treat all JS code as newer version
+
+//alrert(3 + 3)  //we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Nabin")
+
+let name= "nabin"
+let age = 22
+let iskfn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// stirng => ""
+// boolean => true/false
+// undefined =>
+// symbole => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined) //undefined
+
+
