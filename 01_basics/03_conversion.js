04e5c179662fa2802e6910db3980923a5ab19791
nabinbk <113597176+nabinbk@users.noreply.github.com>
2025-02-14 17:58:55 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,27 @@
+//let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//
+// "60" => 60
+// "60abc"=> NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Nabin"=> true
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
