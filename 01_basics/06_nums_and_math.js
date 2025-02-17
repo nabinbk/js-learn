b23e4d2ebdbc92e2d1b8cbd33e2e48f19dc7b33c
nabinbk <113597176+nabinbk@users.noreply.github.com>
2025-02-17 16:15:15 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.1223
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.5));
+// console.log(Math.sqrt(49));
+// console.log(Math.min(12,52,89,45,23,45));
+// console.log(Math.max(12,52,89,45,23,45));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
