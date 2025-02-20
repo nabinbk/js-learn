8fa09b861c4ec747c0b625b6d01ff9a0931435a5
nabinbk <113597176+nabinbk@users.noreply.github.com>
2025-02-20 16:54:44 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -49,11 +49,49 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+
+}
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+
+/* 
+
+// use this bellow code in react 
+
+console.log(instructor);
+
+const navbar = (company) => {
+
+}
+
+navbar(company = "hitesh")
+*/
+
+
+// {
+//     name: "nabin",
+//     coursename: "js in hindi",
+//     price: "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
