9b7699485f2bf02d98b88cfa0f896970802af8fe
nabinbk <113597176+nabinbk@users.noreply.github.com>
2025-03-05 17:48:10 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,34 @@
+// switch (key){
+//     case value:
+
+//     break;
+
+
+//     default:
+//         break;
+// }
+
+const monnth = "March"
+switch (monnth) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("February");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
