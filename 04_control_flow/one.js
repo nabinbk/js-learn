49453d2c950df0e5a65f8d6cb789b8624e3360b4
nabinbk <113597176+nabinbk@users.noreply.github.com>
2025-03-05 17:47:15 +0000

    if else statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,60 @@
+// if
+
+const isUserloggedIn = true
+const temperature = 42
+
+
+// if (temperature === 41) {
+//     console.log("less than 50");
+    
+// } else{
+// console.log("temperature is greater than 50");
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);
+
+// const balance = 800
+
+// if (balance > 500) console.log("test"), console.log("test2");  // not recommnded to use
+
+// if (balance < 500){
+//     console.log("less than 500");
+    
+
+// }else if (balance < 750){
+//     console.log("less than 750");
+    
+// }else if (balance < 900){
+//     console.log("less than 900");
+    
+// }else {
+//     console.log("less than 1200" );
+    
+// }
+
+const userloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userloggedIn && debitCard){
+    console.log("Allow to buy course");
+    
+}if (loggedInFromGoogle|| loggedInFromEmail){
+    console.log("User logged in");
+    
+}
+
+
+
+
+
