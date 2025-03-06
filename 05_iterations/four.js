128f7cde8a722b54d702a7a8394b2916cc7458ba
nabinbk <113597176+nabinbk@users.noreply.github.com>
2025-03-06 18:09:48 +0000

    for in

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "rubby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+    
+}
+
+const programming = ["js", "rb", "py", "java", "cap"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map ()
+// map.set('NEP', "Nepal")
+// map.set('IN', "India")
+// map.set('UK', "United Kingdom")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
